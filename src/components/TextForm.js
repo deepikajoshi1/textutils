d6e0cdc394753b8073b3a8f04e79198168c315ff
deepika joshi <upreti.deepika@gmail.com>
2023-06-12 12:16:11 +0800

    added handleUpClick and HandleOnChange function

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,6 +2,12 @@
 import React, {useState} from 'react';
 
 export default function TextForm(props) {
+  const handleUpClick = ()=>{
+setText("You have clicked on handleUpClick")
+  }
+  const handleOnChange= ()=>{
+
+  }
   // Declare a new state variable, which we'll call "count" and its update-function is setCount.
   const [text, setText] = useState('Enter text here');
 
@@ -11,7 +17,8 @@ export default function TextForm(props) {
         <label for="myBox" class="form-label">
          <h1>{props.heading}</h1>
         </label>
-        <textarea class="form-control" id="myBox" rows="8"></textarea>
+        <textarea class="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
       </div>
     </div>
   );
